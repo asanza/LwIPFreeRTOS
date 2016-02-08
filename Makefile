288e5abd858917111a2773f0d18352c450c831da
Diego F. Asanza <f.asanza@gmail.com>
2016-02-09 00:33:59 +0100

    refactor: reorganize library

---
+++ Makefile
@@ -0,0 +1,25 @@
+#
+#
+#
+
+### Subdirectories
+APPDIR		= #app
+CMSIS			= cmsis
+FREERTOS 	= #freertos
+PLIB			= plib
+
+include common.mk
+
+SUBDIRS = $(APPDIR) $(CMSIS) $(FREERTOS) $(PLIB)
+
+.PHONY: subdirs $(SUBDIRS)
+
+subdirs: $(SUBDIRS)
+
+$(SUBDIRS):
+	$(MAKE) -C $@
+
+clean:
+	for dir in $(SUBDIRS); do \
+		$(MAKE) -C $$dir clean; \
+	done
---
+++ common.mk
@@ -0,0 +1,21 @@
+# Common makefile definitions
+
+### Build flags for all targets
+CFLAGS	= -g -Wall -Wextra -Wundef #-Wstrict-prototypes
+
+# Build tools
+MCU  = cortex-m3
+
+TRGT	= arm-none-eabi-
+CC		= $(TRGT)gcc
+CPPC	= $(TRGT)g++
+LD		= $(TRGT)gcc
+CP		= $(TRGT)objcopy
+AS		= $(TRGT)as
+AR		= $(TRGT)ar
+OD		= $(TRGT)objdump
+SZ		= $(TRGT)size
+HEX		= $(CP) -O ihex
+BIN		= $(CP) -O binary
+
+CFLAGS += -mcpu=$(MCU) -mthumb 
